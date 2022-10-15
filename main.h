9bf93c8996ec486eae1bd0cae790d939d6bdf5f3
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-15 18:29:57 +0100

    new commit

--- main.h
+++ main.h
@@ -3,17 +3,18 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stddef.h>
 
 typedef struct printer
 {
   char *symbol;
-  void (*print)(va_list arg); 
+  int (*print)(va_list arg); 
 } printer_t;
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list);
+int print_char(va_list arg);
+int print_int(va_list arg);
+int print_float(va_list arg);
+int print_string(va_list);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
--- print_functions.c
+++ print_functions.c
@@ -3,6 +3,7 @@
 /**
  * _putchar - prints character
  * @c: character to be printed
+ * Return: int
  */
 
 int _putchar(char c)
@@ -13,11 +14,79 @@ return (write(1, &c, 1));
 /**
  * print_char - prints characters
  * @arg: list of arguments
+ * Return: int
  */
 
-void print_char(va_list arg)
+int print_char(va_list arg)
 {
 char c;
 c = va_arg(arg, char);
 _putchar(c);
+}
+
+/**
+ * print_int - prints integers
+ * @arg: list of arguments
+ * Return: int
+*/
+
+int print_int(va_list arg)
+{
+long int num;
+int count = 0, temp, base;
+num = va_arg(arg, int);
+if (num < 0)
+{
+num *= -1;
+_putchar(45);
+count++;
+}
+if (num >= 0 && num <= 9)
+{
+_putchar(num + 48);
+count++;
+}
+if (num > 9)
+{
+base = 10;
+while (num / base > 9)
+{
+base *= 10;
+}
+while (base > 0)
+{
+temp = num / base;
+num = num % base;
+_putchar(temp + 48);
+base = base / 10;
+count++;
+}
+}
+return (count);
+}
+
+/**
+ * print_string - prints string
+ * @arg: list of arguments
+ * Return: int
+ */
+
+int print_string(va_list arg)
+{
+char *str;
+int count;
+str = va_arg(arg, char*);
+if (str == NULL)
+{
+write(1, "(null)", 6);
+return (6);
+}
+else
+{
+for (count = 0; str[count] != '\0'; count++)
+{
+_putchar(str[count]);
+}
+}
+return (count);
 }
