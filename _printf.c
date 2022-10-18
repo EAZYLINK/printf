b9101c7f8bf6b4eb6ae6e141d4d60a0afca9d9d5
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-10-19 00:10:12 +0100

    new commit

--- _printf.c
+++ _printf.c
@@ -29,7 +29,7 @@ int i, wid, prec, ret = 0;
 char tmp;
 unsigned char flags, len;
 unsigned int (*f)(va_list, buffer_t *,
-unsigned char, int, int, unsigned char);
+unsigned char flags, int wid, int wid, unsigned char len);
 for (i = 0; *(format + i); i++)
 {
 len = 0;
